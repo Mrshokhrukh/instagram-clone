c96d4ebe5d6aa6159f45bac75ad9ad50ec7b251f
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-03-08 18:16:08 +0500

    create post started

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,12 +1,47 @@
 "use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+import useMount from "@/hooks/useMount";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
 function CreatePage({}: Props) {
-  return <div>
-    
-  </div>;
+  const pathname = usePathname();
+  const isCreatePage = pathname === "/dashboard/create";
+  const router = useRouter();
+  const mount = useMount();
+
+  if (!mount) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Dialog
+        open={isCreatePage}
+        onOpenChange={(open) => !open && router.back()}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create new post</DialogTitle>
+          </DialogHeader>
+
+          <Form>
+            <form className="space-y-4">
+                
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
 
 export default CreatePage;
---
+++ hooks/useMount.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function useMount() {
+  const [mount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, [mount]);
+
+  return mount;
+}
+export default useMount;

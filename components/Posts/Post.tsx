e50e1cf600f68118d1a7f9b81d10e2638571d660
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-03-13 23:56:27 +0500

    posts p2

--- components/Posts/Post.tsx
+++ components/Posts/Post.tsx
@@ -2,14 +2,30 @@
 import React from "react";
 import { Card } from "../ui/card";
 import useGetFeedPosts from "@/hooks/useGetFeedPosts";
+import { Avatar } from "@mui/material";
+import Timestamp from "../timestamp/Timestamp";
+
 
 type PostProps = {};
 
 const Post: React.FC<PostProps> = () => {
-  const { posts } = useGetFeedPosts();
+  const { posts, isLoading } = useGetFeedPosts();
+
   return (
-    <div>
-      <Card></Card>
+    <div className="flex flex-col space-y-2.5">
+      <div className="flex items-center justify-between px-3 sm:px-0">
+        <div className="flex space-x-3 items-center">
+          <Avatar src="" className="!w-8 !h-8" />
+          <div className="text-sm">
+            <p className="space-x-1">
+              <span className="font-semibold">shokhrukh</span>
+              <span>+</span>
+              {/* <Timestamp createdAt={new Date()}/> */}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <Card></Card> */}
     </div>
   );
 };
---
+++ components/timestamp/Timestamp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type TimestampProps = {
+  createdAt: Date;
+  className?: string;
+};
+
+const Timestamp: React.FC<TimestampProps> = ({ createdAt, className }) => {
+  return <div>
+    { }
+{/* <ReactTimeago/> */}
+  </div>;
+};
+export default Timestamp;

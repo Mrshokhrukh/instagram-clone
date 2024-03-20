c93ead11e46b704d9c3f7758257cf9fa3d5c5d57
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-03-20 17:53:14 +0500

    ready

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./fireabase/firebase";
 
 export default async function Page() {
-  const [user] = useAuthState(auth);
-  if (user) {
-    localStorage.setItem("userId", JSON.stringify(user?.uid));
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+  redirect("/dashboard");
+  // const [user] = useAuthState(auth);
+  // if (user) {
+  //   localStorage.setItem("userId", JSON.stringify(user?.uid));
+  // } else {
+  //   redirect("/login");
+  // }
 }
